f5ff4c8b007a6c1c73b9ecacb27baa5945c6cebf
hz <gravatar.m2a16@simplelogin.com>
2024-03-03 10:03:57 +0200

    fix build

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/stacks-ogs/nakanoto/",
-  build: {
-    outDir: "../../dist/nakanoto",
-  },
 })
